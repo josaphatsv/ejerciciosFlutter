4488dceacb3c76947190730f31a827cfc45b4050
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-08-10 20:24:00 -0600

    variables booleaneas

---
+++ variablesbooleaneas.dart
@@ -0,0 +1,13 @@
+void main(){
+  //variables booleaneas las variables booleaneas son true false o null
+  bool? isActive = null;
+  
+  if(isActive==null){
+    print('Es un valor null');
+  }else{
+    
+    print('No es nulo');
+    
+  }
+  
+}
