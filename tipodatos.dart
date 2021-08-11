61620c5c78823fd0de01e04edf9011a581d9c761
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-08-10 20:16:36 -0600

    Tipos de datos
    tipo de datos en Dart

---
+++ tipodatos.dart
@@ -0,0 +1,17 @@
+void main(){
+  //string
+  final String nombre = 'Tony';
+  const apellido= 'Stark';
+  
+  //nombre='Peter';
+  
+  //imprime nombre y apellido
+  print('$nombre $apellido');
+  
+  // Numeros
+  int empleados = 10;
+  double salario = 1856.25;
+  
+  print(empleados);
+  print(salario);
+}
